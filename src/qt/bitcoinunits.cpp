ccc8433751c77f8447fb36e025450488de92799c
happy-coin <happyartcoin@gmail.com>
2015-03-24 13:24:46 +0000

    first-changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("HAP");
+    case mBTC: return QString("mHAP");
+    case uBTC: return QString::fromUtf8("μHAP");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("HappyCoins");
+    case mBTC: return QString("Milli-HappyCoins (1 / 1,000)");
+    case uBTC: return QString("Micro-HappyCoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
